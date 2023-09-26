3c0c6ddea1ee85bd03853b4d805eb5fa0ad50435
GerberVll <144966248+GerberVll@users.noreply.github.com>
2023-09-26 17:39:47 -0600

    Agregar archivos de proyecto.

---
+++ Proyec/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace Proyec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer Sonido = new SoundPlayer();
+            Sonido.SoundLocation = "C://Users//Dell Latitude//Documents//UMG//sond//mess.wav";
+            Sonido.Play();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
